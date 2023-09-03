055954ca2d3e3254188475829949a7590c748030
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-03 14:27:22 -0600

    :memo: upadate document

---
+++ ejerciciosBasicosTypeScript/Ejercicio#1.ts
@@ -0,0 +1,31 @@
+// 🧨 Ejercicio 1: Declaración de Variables🧨  //
+/* Declara una variable nombre de tipo string y asígnale tu nombre.
+Declara una variable edad de tipo number y asígnale tu edad.
+Declara una variable esEstudiante de tipo boolean y asígnale true si eres estudiante o false si no lo eres. */
+
+
+// 🧨 Ejercicio 2: Funciones🧨  //
+/* Crea una función llamada saludar que tome un parámetro nombre de tipo string y devuelva un saludo en formato "¡Hola, [nombre]!".
+Llama a la función saludar pasando tu nombre como argumento e imprime el resultado. */
+
+
+
+// 🧨 Ejercicio 3: Tipos de Arrays🧨  //
+/* Declara un array de números llamado numeros y asigna algunos números.
+Declara un array de strings llamado colores y asigna algunos colores.
+Declara un array de booleanos llamado verdaderoFalso y asigna algunos valores true y false. */
+
+
+
+// 🧨 Ejercicio 4: Clases🧨  //
+/* Crea una clase llamada Persona con propiedades nombre y edad.
+Agrega un constructor a la clase Persona que tome los valores de nombre y edad como parámetros y los asigna a las propiedades correspondientes.
+Crea una instancia de la clase Persona con tu nombre y edad.
+Imprime los valores de la instancia creada. */
+
+
+
+// 🧨 Ejercicio 5: Interfaces🧨  //
+/* Define una interfaz llamada Mascota con propiedades nombre de tipo string y edad de tipo number.
+Crea una función llamada mostrarMascota que tome un parámetro de tipo Mascota e imprima sus propiedades en una frase.
+Crea un objeto que cumpla con la interfaz Mascota y pasa ese objeto como argumento a la función mostrarMascota. */
